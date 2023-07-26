8222e2fe0657adadb64b18a2f9d7a544a6cc38f3
Phat <ptnguyen728@gmail.com>
2023-07-26 12:46:14 -0500

    fixed flake 8 except errors

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+
+
 class Technician(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
